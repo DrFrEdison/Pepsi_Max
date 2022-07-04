a563cf28cda280c572060b8cd3cd147f9f77f3db
MPK <kurtz@mailbox.org>
2022-07-04 09:37:59 +0200

    change name in dt_model_overview

--- Q-xx-RMO_00022-V01-00_Pepsi_Max_00300_Acesulfam.R
+++ Q-xx-RMO_00022-V01-00_Pepsi_Max_00300_Acesulfam.R
@@ -165,9 +165,9 @@ keep.out.unsb(model = dt$model.raw, dt$mop$wl1, dt$mop$wl2, dt$mop$wl3, dt$mop$w
 # head(dt$model.overview)
 # 
 # length(which(dt$model.overview$beverage == dt$para$beverage))
-# length(which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]))
+# length(which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]))
 # 
-# if(length(which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ])) == 0){
+# if(length(which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ])) == 0){
 #   dt$model.overview <- rbind(dt$model.overview
 #                              , data.frame(customer = dt$para$customer
 #                                           , location = NA
@@ -185,17 +185,17 @@ keep.out.unsb(model = dt$model.raw, dt$mop$wl1, dt$mop$wl2, dt$mop$wl3, dt$mop$w
 #   )
 # }
 # 
-# if(length(which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ])) == 1){
+# if(length(which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ])) == 1){
 # 
-#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]) , "wl1"] <- dt$mop$wl1
-#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]) , "wl2"] <- dt$mop$wl2
-#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]) , "wl3"] <- dt$mop$wl3
-#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]) , "wl4"] <- dt$mop$wl4
-#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]) , "PC"] <- dt$mop$ncomp
-#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$Parameter == dt$para$substance[ dt$para$i ]) , "transform"] <- dt$mop$spc
+#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]) , "wl1"] <- dt$mop$wl1
+#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]) , "wl2"] <- dt$mop$wl2
+#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]) , "wl3"] <- dt$mop$wl3
+#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]) , "wl4"] <- dt$mop$wl4
+#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]) , "ncomp"] <- dt$mop$ncomp
+#   dt$model.overview[which(dt$model.overview$beverage == dt$para$beverage & dt$model.overview$substance == dt$para$substance[ dt$para$i ]) , "spc"] <- dt$mop$spc
 # }
 # 
-# dt$model.overview <- dt$model.overview[ order(dt$model.overview$customer, dt$model.overview$beverage, dt$model.overview$Parameter),]
+# dt$model.overview <- dt$model.overview[ order(dt$model.overview$customer, dt$model.overview$beverage, dt$model.overview$substance),]
 # write_ods(x = dt$model.overview, path = "dt_model_overview.ods", overwrite = T)
 # 
 # setwd("./model")
